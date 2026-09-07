c5f44849404a36eab49a1d9a3b0806b2110b87a2
rohitsingh0006 <singhrohit0047@gmail.com>
2026-09-07 12:04:01 +0530

    updated address_prefixes to address_space in vnet main.tf

--- virtual_networks/01_main.tf
+++ virtual_networks/01_main.tf
@@ -5,5 +5,5 @@ resource "azurerm_virtual_network" "vnet" {
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
   location            = each.value.location
-  address_prefixes    = each.value.address_prefixes
+  address_space    = each.value.address_space
 }
