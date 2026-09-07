03d232909b914921aeedba43536afe88dfaec816
rohitsingh0006 <singhrohit0047@gmail.com>
2026-09-07 12:22:00 +0530

    updated rg output.tf for name and location

--- resource_groups/03_outputs.tf
+++ resource_groups/03_outputs.tf
@@ -5,3 +5,15 @@ output "resource_group_id" {
     for key, rg in azurerm_resource_group.rg : key => rg.id
   }
 }
+
+output "resource_group_name" {
+  value = {
+    for key, rg in azurerm_resource_group.rgs : key => rg.name
+  }
+}
+
+output "resource_group_location" {
+  value = {
+    for key, rg in azurerm_resource_group.rgs : key => rg.location
+  }
+}
