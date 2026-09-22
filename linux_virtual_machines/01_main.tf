1d4e65c71ae7eb55dc34df48a40d53b6d3e295e9
rohitsingh0006 <singhrohit0047@gmail.com>
2026-09-22 15:37:43 +0530

    modified main.tf for module linux vm

--- linux_virtual_machines/01_main.tf
+++ linux_virtual_machines/01_main.tf
@@ -4,8 +4,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   resource_group_name = each.value.resource_group_name
   location            = each.value.location
   size                = each.value.size
-  admin_username      = data.azurerm_key_vault_secret.vm_admin_username[each.key].value
-  admin_password      = data.azurerm_key_vault_secret.vm_admin_password[each.key].value
+  admin_username      = each.value.admin_username
+  admin_password      = each.value.admin_password
   disable_password_authentication = each.value.disable_password_authentication
   network_interface_ids = [data.azurerm_network_interface.nic_ids[each.key].id]
 
