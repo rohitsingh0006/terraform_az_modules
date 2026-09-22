599c6d08015213b285ed31e39acaddd076da5be4
rohitsingh0006 <singhrohit0047@gmail.com>
2026-09-22 15:14:04 +0530

    added module linux virtual machine

---
+++ linux_virtual_machine/01_main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  for_each            = var.vms
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+  location            = each.value.location
+  size                = each.value.size
+  admin_username      = data.azurerm_key_vault_secret.vm_admin_username[each.key].value
+  admin_password      = data.azurerm_key_vault_secret.vm_admin_password[each.key].value
+  disable_password_authentication = each.value.disable_password_authentication
+  network_interface_ids = [data.azurerm_network_interface.nic_ids[each.key].id]
+
+   dynamic "os_disk" {
+    for_each = each.value.os_disk
+    content {
+    name                 = os_disk.value.name
+    caching              = os_disk.value.caching
+    storage_account_type = os_disk.value.storage_account_type
+#    disk_size_gb         = os_disk.value.disk_size_gb
+    }
+   }
+   
+source_image_reference {
+    publisher = each.value.source_image_reference.publisher
+    offer     = each.value.source_image_reference.offer
+    sku       = each.value.source_image_reference.sku
+    version   = each.value.source_image_reference.version
+  }
+}
---
+++ linux_virtual_machine/02_variables.tf
@@ -0,0 +1,50 @@
+# Virtual Machine (Linux)
+variable "vms" {
+  description = "Map of Linux Virtual Machine configurations"
+  type = map(object({
+    # data block variable
+    nic_name                = string
+#    kv_name                 = string
+    vm_username_secret_name = string
+    vm_password_secret_name = string
+    # resource block variable
+    name                = string
+    resource_group_name = string
+    location            = string
+    size                = string
+    # costom_data script_name
+#    script_name         = string
+    # admin_username      = string                # value taken from key vault secret
+    # admin_password      = optional(string)      # value taken from key vault secret
+    # network_interface_ids           = list(string)  # if we using data block for this then not required here
+    disable_password_authentication = optional(bool)
+    computer_name                   = optional(string)
+    custom_data                     = optional(string)
+    provision_vm_agent              = optional(bool)
+    allow_extension_operations      = optional(bool)
+    edge_zone                       = optional(string)
+    encryption_at_host_enabled      = optional(bool)
+    patch_mode                      = optional(string)
+    reboot_setting                  = optional(string)
+    secure_boot_enabled             = optional(bool)
+    vtpm_enabled                    = optional(bool)
+    tags                            = optional(map(string))
+    zone                            = optional(string)
+
+    
+    os_disk = optional(list(object({
+      name                 = string
+      caching              = string
+      storage_account_type = string
+      disk_size_gb         = optional(number)
+    })), [])
+
+
+    source_image_reference = object({
+      publisher = string
+      offer     = string
+      sku       = string
+      version   = string
+    })
+
+}
