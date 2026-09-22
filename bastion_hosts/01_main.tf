75a7606380619328c3c29e3aef5b356088b66991
rohitsingh0006 <singhrohit0047@gmail.com>
2026-09-22 16:02:26 +0530

    added module bastion hosts

---
+++ bastion_hosts/01_main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_bastion_host" "bas" {
+  for_each            = var.bhs
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+
+  dynamic ip_configuration {
+    for_each             = each.value.ip_configuration
+    content {
+    name                 = ip_configuration.value.name
+    subnet_id            = data.azurerm_subnet.subnet_ids[each.key].id
+    public_ip_address_id = data.azurerm_public_ip.pip_ids[each.key].id
+
+    }
+  }
+}
