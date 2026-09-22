88ec246f6efd72549b8a8492aed0a3201d2d5179
rohitsingh0006 <singhrohit0047@gmail.com>
2026-09-22 14:10:57 +0530

    added module network interfaces

---
+++ network_interfaces/01_main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_network_interface" "nic" {
+  for_each = var.nics
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+
+ dynamic "ip_configuration" {
+    for_each                      = each.value.ip_configurations
+    content {
+     name                         = ip_configuration.value.name
+    subnet_id                     =  data.azurerm_subnet.subnet_ids[each.key].id
+    private_ip_address_allocation =  ip_configuration.value.private_ip_address_allocation
+    public_ip_address_id          =  data.azurerm_public_ip.pip_ids[each.key].id
+    primary                       =  ip_configuration.value.primary
+    }
+ }
+ 
+ 
+ 
+  
+}
